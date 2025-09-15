05fbe46255d99164493500247bf39aed105f1181
ChipsAhoiMcCoy <coltoncmeyer@icloud.com>
2025-09-15 15:55:12 -0700

    Add AccessibilityMod.cs main mod class skeleton

---
+++ AccessibilityMod/AccessibilityMod.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+
+namespace AccessibilityMod
+{
+    public class AccessibilityMod : Mod
+    {
+        public AccessibilityMod()
+        {
+            // You can add initialization code here if needed.
+        }
+    }
+}
